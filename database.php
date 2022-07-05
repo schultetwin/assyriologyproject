3ed0ede355f253fad84f20518046751d95f00efe
Mark Schulte <schultetwin@gmail.com>
2022-07-05 02:51:52 -0400

    Reference globals

--- database.php
+++ database.php
@@ -3,6 +3,7 @@
 require('db_params.php')
 
 function getMysqliConnection() {
+    global $db_host, $db_user, $db_pass, $db_name;
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, '3308');
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
