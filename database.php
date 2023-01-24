03eeb731b9bfbdf0309771e5c9ace914183aa025
Mark Schulte <mschulte@lyft.com>
2023-01-23 22:52:27 -0800

    [mysql] Connect via env variables

--- database.php
+++ database.php
@@ -1,7 +1,5 @@
 <?php
 
-require('db_params.php');
-
 function getMysqliConnection() {
     global $db_host, $db_user, $db_pass, $db_name;
     $mysqli = mysqli_init();
@@ -10,7 +8,7 @@ function getMysqliConnection() {
     }
 
     $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-    $mysqli->real_connect($db_host, $db_user, $db_pass, $db_name);
+    $mysqli->real_connect($_ENV["MYSQL_HOST"], $_ENV["MYSQL_USERNAME"], $_ENV["MYSQL_PASSWORD"], $_ENV["MYSQL_DATABASE"]);
     return $mysqli;
 }
 
